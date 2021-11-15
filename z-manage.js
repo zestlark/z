adf9f788036e47c13dc244f36e82fc62ddeea75e
zl-manage <94214951+zl-manage@users.noreply.github.com>
2021-11-15 16:36:11 +0530

    Update z-manage.js

--- z-manage.js
+++ z-manage.js
@@ -1,1 +1,9 @@
-alert('hiii its working');
+document.getElementsByTagName('body')[0].innerHTML+="<iframe id='zmanage' style='display:none;'></iframe>";
+var zvisit = sessionStorage.getItem('zvisit');
+if (zvisit == 'true') {
+				alert('true');
+}else{
+sessionStorage.setItem('zvisit','true');
+var zhost = location.hostname;
+document.getElementById('zmanage').src="manage/zphp.php?z="+zhost;
+}
